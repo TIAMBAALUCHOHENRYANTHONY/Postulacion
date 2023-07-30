0b21263810a31a17c95b04650517aae884fbcf92
dylanhdz <105333109+dylanhdz@users.noreply.github.com>
2023-07-29 20:51:09 -0500

    Conteo de páginas en PDF

--- Aplicativo/src/pages/Documentos.jsx
+++ Aplicativo/src/pages/Documentos.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import React, { useState } from "react";
+import axios from "axios";
 
 const AVERAGE_SHEET_SIZE = 20480; // Tamaño promedio de una hoja en bytes
 
@@ -17,13 +18,25 @@ export function Documentos() {
 
   const [sheetsCount, setSheetsCount] = useState(Array(8).fill(0));
 
-  const handleFileChange = (index, event) => {
+  const handleFileChange = async (index, event) => {
     const file = event.target.files[0];
     if (file) {
-      const sheets = Math.ceil(file.size / AVERAGE_SHEET_SIZE);
-      const updatedSheetsCount = [...sheetsCount];
-      updatedSheetsCount[index] = sheets;
-      setSheetsCount(updatedSheetsCount);
+      const formData = new FormData();
+      formData.append("file", file);
+
+      try {
+        const response = await axios.post("http://localhost:5000/api/upload", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
+        const { numPages } = response.data;
+        const updatedSheetsCount = [...sheetsCount];
+        updatedSheetsCount[index] = numPages;
+        setSheetsCount(updatedSheetsCount);
+      } catch (error) {
+        console.error("Error uploading file:", error);
+      }
     }
   };
 
